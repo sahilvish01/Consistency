7c4d48592e9d7974f9b009edd51a83f5bc5b48fa
sahilvish01 <sahilvish6542@gmail.com>
2025-10-11 03:50:13 +0530

    switch-case

---
+++ c12.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n=1;
+    switch(n)
+    {
+        case 1 : printf("Consistency");
+                break;
+        case 2 : printf("Consistency");
+                break;
+        default : printf("To bhi consistency");
+    }
+}
